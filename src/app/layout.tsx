4735add5d3920abd35f6dd4bc4bf00b0f57ae199
wvsr <email.wasimul@gmail.com>
2025-08-31 00:37:48 +0600

    chore: temporarily remove custom font for debugging

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,7 @@
 import type { Metadata } from 'next'
-import { Merriweather } from 'next/font/google'
 import './globals.css'
 import Nav from '@/components/nav'
 import { ThemeProvider } from '@/components/theme-provider'
-
-const merriweather = Merriweather({
-  subsets: ['latin'],
-  weight: ['400', '700'],
-})
 
 export const metadata: Metadata = {
   title: 'Wasimul Sami',
@@ -20,7 +14,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={merriweather.className}>
+      <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <div className="mx-auto w-[750px] max-w-full px-5 pb-5 pt-10 text-text dark:text-darkText">
             <Nav />
